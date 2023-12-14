bcde54b5c42708a3096d0d23a148f6d2e12657f0
User <User@DESKTOP-B8Q1GVC>
2023-12-14 12:30:18 -0500

    Add project files.

---
+++ ReadingAndWritingToAnCSVFile/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ReadingAndWritingToAnCSVFile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string Ages = "C:\\Users\\User\\source\\repos\\ReadingAndWritingToAnCSVFile\\ReadingAndWritingToAnCSVFile\\Sample_Data_Age.csv";
+            string AgesTo = "C:\\Users\\User\\source\\repos\\ReadingAndWritingToAnCSVFile\\ReadingAndWritingToAnCSVFile\\Numbers.csv";
+
+            string[] lines = File.ReadAllLines(Ages);
+
+            foreach (string line in lines)
+            {
+                string[] values = line.Split(',');
+
+                foreach (string value in values)
+                {
+                    if (double.TryParse(value, out double numericValue) && numericValue > 65)
+                    {
+                        File.AppendAllText(AgesTo, line + Environment.NewLine);
+                    }
+                }
+            }
+        }
+    }
+}
